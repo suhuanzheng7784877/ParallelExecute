b505276933e3665fdc6c42b011dae913a7150742
suhuanzheng7784877 <suhuanzheng7784877@163.com>
2013-11-21 20:51:44 +0800

    update

--- project/ParallelExecute/src/main/java/org/para/distributed/master/DistributedParallelExecute.java
+++ project/ParallelExecute/src/main/java/org/para/distributed/master/DistributedParallelExecute.java
@@ -25,15 +25,7 @@ import org.para.util.MessageOutUtil;
  * @param <T>
  */
 public abstract class DistributedParallelExecute<T extends Serializable> {
-	
-<<<<<<< HEAD
-	
-	
-	
-=======
 
-
->>>>>>> 0f9fbb7e7345c928135127e8ef087d67d6b15c50
 	public CountDownLatch countDownLatch = null;
 
 	/**
